63721d2f67af65c21a076a617324fbd60c7ae1c6
sephineezx <phineeezx@gmail.com>
2026-06-04 20:10:51 +0700

    Rapikan getter setter DetailTransaksi

--- DetailTransaksi.java
+++ DetailTransaksi.java
@@ -10,20 +10,53 @@ public class DetailTransaksi {
     private BigDecimal diskonItem;
     private BigDecimal subtotal;
 
-    public int getId()                      { return id; }
-    public void setId(int id)               { this.id = id; }
-    public int getTransaksiId()             { return transaksiId; }
-    public void setTransaksiId(int i)       { this.transaksiId = i; }
-    public int getProdukId()                { return produkId; }
-    public void setProdukId(int i)          { this.produkId = i; }
-    public String getNamaProduk()           { return namaProduk; }
-    public void setNamaProduk(String n)     { this.namaProduk = n; }
-    public BigDecimal getHargaSatuan()      { return hargaSatuan; }
-    public void setHargaSatuan(BigDecimal h){ this.hargaSatuan = h; }
-    public int getQty()                     { return qty; }
-    public void setQty(int q)               { this.qty = q; }
-    public BigDecimal getDiskonItem()       { return diskonItem; }
-    public void setDiskonItem(BigDecimal d) { this.diskonItem = d; }
-    public BigDecimal getSubtotal()         { return subtotal; }
-    public void setSubtotal(BigDecimal s)   { this.subtotal = s; }
+    public int getId()                      { 
+        return id; 
+    }
+    public void setId(int id)               
+    { this.id = id; 
+
+    }
+    public int getTransaksiId()             { 
+        return transaksiId; 
+    }
+    public void setTransaksiId(int i)       { 
+        this.transaksiId = i; 
+    }
+    public int getProdukId()                { 
+        return produkId; 
+    }
+    public void setProdukId(int i)          { 
+        this.produkId = i; 
+    }
+    public String getNamaProduk()           { 
+        return namaProduk; 
+    }
+    public void setNamaProduk(String n)     { 
+        this.namaProduk = n; 
+    }
+    public BigDecimal getHargaSatuan()      { 
+        return hargaSatuan; 
+    }
+    public void setHargaSatuan(BigDecimal h){ 
+        this.hargaSatuan = h; 
+    }
+    public int getQty()                     { 
+        return qty; 
+    }
+    public void setQty(int q)               { 
+        this.qty = q; 
+    }
+    public BigDecimal getDiskonItem()       { 
+        return diskonItem; 
+    }
+    public void setDiskonItem(BigDecimal d) { 
+        this.diskonItem = d; 
+    }
+    public BigDecimal getSubtotal()         { 
+        return subtotal; 
+    }
+    public void setSubtotal(BigDecimal s)   { 
+        this.subtotal = s; 
+    }
 }
