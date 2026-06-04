03e8ecb91c63c9dd0d2de5193b74da860d636f67
sephineezx <phineeezx@gmail.com>
2026-06-04 20:26:29 +0700

    Produk

--- Produk.java
+++ Produk.java
@@ -1,8 +1,4 @@
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Produk {
     private int id;
@@ -32,26 +28,66 @@ public class Produk {
         this.aktif = aktif;
     }
 
-    public int getId()                   { return id; }
-    public void setId(int id)            { this.id = id; }
-    public String getKode()              { return kode; }
-    public void setKode(String kode)     { this.kode = kode; }
-    public String getNama()              { return nama; }
-    public void setNama(String nama)     { this.nama = nama; }
-    public int getKategoriId()           { return kategoriId; }
-    public void setKategoriId(int id)    { this.kategoriId = id; }
-    public String getKategoriNama()      { return kategoriNama; }
-    public void setKategoriNama(String n){ this.kategoriNama = n; }
-    public BigDecimal getHarga()         { return harga; }
-    public void setHarga(BigDecimal h)   { this.harga = h; }
-    public int getStok()                 { return stok; }
-    public void setStok(int s)           { this.stok = s; }
-    public String getSatuan()            { return satuan; }
-    public void setSatuan(String s)      { this.satuan = s; }
-    public String getDeskripsi()         { return deskripsi; }
-    public void setDeskripsi(String d)   { this.deskripsi = d; }
-    public boolean isAktif()             { return aktif; }
-    public void setAktif(boolean a)      { this.aktif = a; }
+    public int getId()                   { 
+        return id; 
+    }
+    public void setId(int id)            { 
+        this.id = id; 
+    }
+    public String getKode()              { 
+        return kode; 
+    }
+    public void setKode(String kode)     { 
+        this.kode = kode; 
+    }
+    public String getNama()              { 
+        return nama; 
+    }
+    public void setNama(String nama)     { 
+        this.nama = nama; 
+    }
+    public int getKategoriId()           { 
+        return kategoriId; 
+    }
+    public void setKategoriId(int id)    { 
+        this.kategoriId = id; 
+    }
+    public String getKategoriNama()      { 
+        return kategoriNama; 
+    }
+    public void setKategoriNama(String n){ 
+        this.kategoriNama = n; 
+    }
+    public BigDecimal getHarga()         { 
+        return harga; 
+    }
+    public void setHarga(BigDecimal h)   { 
+        this.harga = h; 
+    }
+    public int getStok()                 { 
+        return stok; 
+    }
+    public void setStok(int s)           { 
+        this.stok = s; 
+    }
+    public String getSatuan()            { 
+        return satuan; 
+    }
+    public void setSatuan(String s)      { 
+        this.satuan = s; 
+    }
+    public String getDeskripsi()         { 
+        return deskripsi; 
+    }
+    public void setDeskripsi(String d)   { 
+        this.deskripsi = d; 
+    }
+    public boolean isAktif()             { 
+        return aktif; 
+    }
+    public void setAktif(boolean a)      { 
+        this.aktif = a; 
+    }
 
     @Override
     public String toString() { return nama; }
